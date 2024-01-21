78c02b17d50c4d70fd01590ce520a0aebdfd3f4b
Your Name <thepomidorik@gmail.com>
2024-01-22 02:04:10 +0300

    init

---
+++ public/video_three_adv/video_script.js
@@ -0,0 +1,16 @@
+alert('fomvoefvno')
+
+document.addEventListener("DOMContentLoaded", function () {
+    // Получаем элемент видео
+    var videoElement = document.getElementById("my-video");
+
+    // Получаем путь к видео из атрибута src
+    var videoPath = videoElement.getAttribute("src");
+    console.log("Путь к видео:", videoPath);
+
+
+    var url = new URL(window.location.href);
+    var videoCode = url.searchParams.get("videoCode");
+
+    console.log(videoCode);
+});
